d3eed227604c80b7ad11f5865f46d357d28a568d
Colin Jack <245655340@qq.com>
2016-12-04 01:50:35 +0800

    2016.11.30_Create Project
    Create Project With Upload All FIles

---
+++ ForInstall/ForInstall.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ForInstall
+{
+    partial class ForInstall
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ShuMin = new System.Windows.Forms.Label();
+            this.BTN_Install = new System.Windows.Forms.Button();
+            this.Notice = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ShuMin
+            // 
+            this.ShuMin.AutoSize = true;
+            this.ShuMin.Location = new System.Drawing.Point(12, 55);
+            this.ShuMin.Name = "ShuMin";
+            this.ShuMin.Size = new System.Drawing.Size(41, 12);
+            this.ShuMin.TabIndex = 0;
+            this.ShuMin.Text = "说明：";
+            // 
+            // BTN_Install
+            // 
+            this.BTN_Install.Location = new System.Drawing.Point(120, 15);
+            this.BTN_Install.Name = "BTN_Install";
+            this.BTN_Install.Size = new System.Drawing.Size(75, 25);
+            this.BTN_Install.TabIndex = 1;
+            this.BTN_Install.Text = "安装";
+            this.BTN_Install.UseVisualStyleBackColor = true;
+            this.BTN_Install.Click += new System.EventHandler(this.BTN_Install_Click);
+            // 
+            // Notice
+            // 
+            this.Notice.AutoSize = true;
+            this.Notice.Location = new System.Drawing.Point(73, 55);
+            this.Notice.Name = "Notice";
+            this.Notice.Size = new System.Drawing.Size(125, 12);
+            this.Notice.TabIndex = 2;
+            this.Notice.Text = "请点击安装来安装程序";
+            // 
+            // ForInstall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 92);
+            this.Controls.Add(this.Notice);
+            this.Controls.Add(this.BTN_Install);
+            this.Controls.Add(this.ShuMin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "ForInstall";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "键盘控制安装程序";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ShuMin;
+        private System.Windows.Forms.Button BTN_Install;
+        private System.Windows.Forms.Label Notice;
+    }
+}
+
---
+++ ForInstall/ForInstall.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.IO;
+using Microsoft.Win32;
+
+namespace ForInstall
+{
+    public partial class ForInstall : Form
+    {
+        public ForInstall()
+        {
+            InitializeComponent();
+        }
+        //////////////////////////
+        ////////参数定义区域///////
+        //////////////////////////
+        string para_sharedIP = "10.16.139.81"; //共享服务器IP地址
+        string para_netLoginPath = " use \\\\10.16.139.81\\Shared  /user:\"User\" \"Passwd\"";  //共享服务器的共享登录信息
+        string para_netMainProgramFilePath = @"\\10.16.139.81\Shared\KeyboardIntercept.exe"; //共享服务器中主程序路径
+        string para_localMainProgramFilePath = "C:\\Windows\\System32\\KeyboardIntercept.exe";  //本地主程序将要存储的路径
+        string para_MainProgramFileName = "KeyboardIntercept.exe";  //主程序exe设定的全名称
+        int para_currentNetwork = 0;
+        //////////////////////////
+        ////////参数定义区域///////
+        //////////////////////////
+        private void BTN_Install_Click(object sender, EventArgs e)
+        {
+            string localMainProgramFile = System.Environment.CurrentDirectory + para_MainProgramFileName;
+            if (File.Exists(localMainProgramFile)) {
+                try {
+                    File.Copy(localMainProgramFile, para_localMainProgramFilePath, true);
+                    Notice.Text = "本地程序拷贝成功,将开始配置.";
+                    Boolean writeOrNot = writeIntoRegedit("System", para_localMainProgramFilePath);
+                    if (writeOrNot)
+                    {
+                        System.Diagnostics.Process.Start(para_localMainProgramFilePath);
+                        Notice.Text = "安装成功,配置成功.";
+                    }
+                    else { Notice.Text = "配置失败,请联系管理员!"; }
+                    return;
+                }
+                catch (IOException ex)
+                { //System.Console.WriteLine(ex.ToString()); 
+                    Notice.Text = "本地程序拷贝失败,将要启动远程拷贝功能.";
+                    return;
+                }
+            }
+            networkStatusJudge(para_sharedIP);
+            if (para_currentNetwork == 1)
+            {
+                try {
+                    System.Diagnostics.Process.Start("net.exe", para_netLoginPath);
+                    if (File.Exists(para_netMainProgramFilePath))
+                    {
+                        File.Copy(para_netMainProgramFilePath, para_localMainProgramFilePath, true);
+                        Notice.Text = "远程文件拷贝成功,将要开始配置.";
+                        Boolean writeOrNot = writeIntoRegedit("System", para_localMainProgramFilePath);
+                        if (writeOrNot)
+                        {
+                            System.Diagnostics.Process.Start(para_localMainProgramFilePath);
+                            Notice.Text = "安装成功,配置成功.";
+                        }
+                    }
+                    else {
+                        Notice.Text = "配置失败,请联系管理员!";
+                        return;
+                    }
+                }catch(IOException ex){
+                    System.Console.WriteLine(ex.ToString());
+                }
+            }
+            else {
+                Notice.Text = "程序安装失败,请检查网络状态。";
+                //System.Console.WriteLine("File is no exists , Please Contact Administrator");
+            }
+        }
+        /// <summary>
+        /// 将程序在注册中设置为自动启动
+        /// </summary>
+        /// <param name="keyName"></param>
+        /// <param name="keyValue"></param>
+        /// <returns></returns>
+        private bool writeIntoRegedit(string keyName, string keyValue)
+        {
+            RegistryKey regKey = Registry.LocalMachine;
+            Boolean itemExist = isRegeditItemExist(@"SOFTWARE\Microsoft\Windows\CurrentVersion","Run");
+            if (itemExist == true) {
+                itemExist = isRegeditKeyExist(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run","System");
+                if (itemExist == true)
+                {
+                    RegistryKey delAddKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+                    delAddKey.DeleteValue("System");
+                    delAddKey.SetValue("System", para_localMainProgramFilePath);
+                    delAddKey.Close();
+                }
+                else {
+                    RegistryKey delAddKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+                    delAddKey.SetValue("System", para_localMainProgramFilePath);
+                    delAddKey.Close();
+                }
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// 判断注册表目录项是否存在
+        /// </summary>
+        /// <param name="regeditPath"></param>
+        /// <param name="regeditItem"></param>
+        /// <returns></returns>
+        private bool isRegeditItemExist(string regeditPath,string regeditItem)
+        {
+            string[] subkeyNames;
+            RegistryKey hkml = Registry.LocalMachine;
+            RegistryKey CurrentVersion = hkml.OpenSubKey(regeditPath);
+            //RegistryKey software = hkml.OpenSubKey("SOFTWARE", true);
+            subkeyNames = CurrentVersion.GetSubKeyNames();
+            //取得该项下所有子项的名称的序列，并传递给预定的数组中
+            foreach (string keyName in subkeyNames)  //遍历整个数组
+            {
+                if (keyName == regeditItem) //判断子项的名称
+                {
+                    hkml.Close();
+                    return true;
+                }
+            }
+            hkml.Close();
+            return false;
+        }
+        /// <summary>
+        /// 判断注册表指定目录中的指定字段值是否存在
+        /// </summary>
+        /// <param name="regeditPath"></param>
+        /// <param name="regeditItem"></param>
+        /// <returns></returns>
+        private bool isRegeditKeyExist(string regeditPath, string regeditItem)
+        {
+            string[] subkeyNames;
+            RegistryKey hkml = Registry.LocalMachine.OpenSubKey(regeditPath);
+            //RegistryKey software = hkml.OpenSubKey("SOFTWARE\\Run", true);
+            subkeyNames = hkml.GetValueNames();
+            //取得该项下所有键值的名称的序列，并传递给预定的数组中
+            foreach (string keyName in subkeyNames)
+            {
+                if (keyName == regeditItem)  //判断键值的名称
+                {
+                    hkml.Close();
+                    return true;
+                }
+            }
+            hkml.Close();
+            return false;
+        }
+        /// <summary>
+        /// 判断网络状况
+        /// </summary>
+        /// <param name="parain_IP"></param>
+        private void networkStatusJudge(string parain_IP)
+        {
+            Ping ping = new Ping();
+            PingReply pingReply = ping.Send(parain_IP);
+            if (pingReply.Status == IPStatus.Success) {
+                //Console.WriteLine("OnLine,ping Success!");
+                para_currentNetwork = 1;
+            }
+            else {
+                //Console.WriteLine("Offline，ping Failed!");
+                para_currentNetwork = 0;
+            }
+        }
+    }
+}
